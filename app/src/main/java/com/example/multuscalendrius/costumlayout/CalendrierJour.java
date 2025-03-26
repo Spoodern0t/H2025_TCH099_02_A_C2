0cc88848e2d8661b519bdc0cdfb5af900496fc1e
Jimmy_Allard <allardjimmy1@hotmail.com>
2025-03-26 13:55:09 -0400

    Added: View calendrierJour

---
+++ app/src/main/java/com/example/multuscalendrius/costumlayout/CalendrierJour.java
@@ -0,0 +1,26 @@
+package com.example.multuscalendrius.costumlayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CalendrierJour extends View {
+
+    public CalendrierJour(Context context) {
+        super(context);
+    }
+
+    public CalendrierJour(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CalendrierJour(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CalendrierJour(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
