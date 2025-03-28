06e20669161a624ad0ffb8ffae03ba77fa694e31
Tomas Hajj Assaf <159832462+tom-hajj@users.noreply.github.com>
2025-03-28 17:13:48 -0400

    Update app/src/main/java/com/example/multuscalendrius/modeles/entitees/Calendrier.java
    Co-authored-by: JimmyAllard <154572162+JimmyAllard@users.noreply.github.com>

--- app/src/main/java/com/example/multuscalendrius/modeles/entitees/Calendrier.java
+++ app/src/main/java/com/example/multuscalendrius/modeles/entitees/Calendrier.java
@@ -24,7 +24,7 @@ public class Calendrier {
 
     // Getters & Setters
     public int getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public void setId(int id) { this.id = id; }
 
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
